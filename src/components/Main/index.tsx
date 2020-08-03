9daf091335d1d8663a1c76df3773d11ba85eb37a
Mauro Rocha <mauror@mauror.com.br>
2020-08-02 22:09:51 -0300

    boilerplate done

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,18 @@
-const Main = () => (
-  <main>
-    <h1>React Avancado</h1>
-  </main>
+import * as S from './styles'
+
+const Main = ({
+  title = 'React Avancado',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
+  <S.Wrapper>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem da logomarca do react como exemplo"
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration src="/img/hero-illustration.svg" />
+  </S.Wrapper>
 )
 
 export default Main
